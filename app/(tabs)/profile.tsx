4a04b95572f1a9936bee0f151c732f9a233ed009
nival <nivahmichael@gmail.com>
2026-01-16 22:35:23 +0300

    updated profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,15 @@
-import { icons } from '@/constants/icons'
-import React from 'react'
-import { Image, Text, View } from 'react-native'
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, Text, View } from "react-native";
 
 const profile = () => {
   return (
     <View className="flex-1 bg-primary px-10">
       <View className="flex justify-center items-center flex-1 flex-col gap-5">
-        <Image source={icons.person} className="size-10" tintColor="#fff"/>
+        <Image source={icons.person} className="size-10" tintColor="#fff" />
         <Text className="text-gray-500 text-base">Profile</Text>
       </View>
     </View>
-  )
-}
-
-export default profile
+  );
+};
+export default profile;
