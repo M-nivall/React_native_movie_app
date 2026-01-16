2ad02dcfea4e529455b55c82f43175c8cce74179
nival <nivahmichael@gmail.com>
2026-01-16 22:28:55 +0300

    implemented trending movie card

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,21 +1,28 @@
-import { Link } from 'expo-router';
-import React from 'react';
-import { Image, Text, TouchableOpacity } from 'react-native';
+import { Link } from "expo-router";
+import React from "react";
+import { Image, Text, TouchableOpacity } from "react-native";
 
-const TrendingCard = ({movie:{movie_id, title, poster_url}, index}: TrendingCardProps) => {
+const TrendingCard = ({
+  movie: { movie_id, title, poster_url },
+  index,
+}: TrendingCardProps) => {
   return (
     <Link href={`/movies/${movie_id}`} asChild>
-        <TouchableOpacity className="w-32 relative pl-5">
-            <Image 
-                source={{uri: poster_url}}
-                className="w-32 h-48 rounded-lg"
-                resizeMode="cover"
-            />
+      <TouchableOpacity className="w-32 relative pl-5">
+        <Image
+          source={{ uri: poster_url }}
+          className="w-32 h-48 rounded-lg"
+          resizeMode="cover"
+        />
 
-            <Text className="text-sm font-blod mt-2 text-light-200" numberOfLines={1}>{title}</Text>
-        </TouchableOpacity>
+        <Text
+          className="text-sm font-blod mt-2 text-light-200"
+          numberOfLines={1}
+        >
+          {title}
+        </Text>
+      </TouchableOpacity>
     </Link>
-  )
-}
-
-export default TrendingCard
+  );
+};
+export default TrendingCard;
